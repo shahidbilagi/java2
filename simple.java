ac9555a292ce1d1a933ff64f65a8ee4cf78daff4
shahidbilagi <shahidbilagi7@gmail.com>
2025-04-11 10:21:19 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,11 @@
+public class simple {
+    public static void main(String[] args) {
+        int x=5;
+        int y=6;
+        System.out.println("Sum is "+(x+y));
+        System.out.println("Difference is "+(x-y));
+        System.out.println("Product is "+(x*y));
+        System.out.println("quotient is "+(x/y));
+        System.out.println("reminder is "+(y%x));
+    }
+}
